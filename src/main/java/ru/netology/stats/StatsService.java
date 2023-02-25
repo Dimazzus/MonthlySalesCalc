983ecbe202a5b53ecb1863fbc49171a391bcee22
Your Name <you@example.com>
2023-02-25 12:51:07 +1200

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,81 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public int minSales(int[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int maxSales(int[] sales) {
+        int maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+
+        return maxMonth + 1; // номер месяца с максимальным количеством продаж
+    }
+
+
+    public int amountSales(int[] sales) {
+        int sum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+        }
+        return sum;// общее количество продаж
+    }
+
+    public int averageSales(int[] sales) {
+
+        double average = 0;
+        if (sales.length > 0) {
+            double sum = 0;
+            for (int i = 0; i < sales.length; i++) {
+                sum += sales[i];
+            }
+            average = sum / sales.length;
+        }
+        return (int) average;// среднее значение продаж
+    }
+
+    public int salesUnderAverage(int[] sales) {
+        int underAverage = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= 15) {
+                continue;
+            } else {
+
+                underAverage += 1;// количество месяцев продаж ниже среднего значения
+            }
+        }
+
+        return underAverage;
+
+    }
+
+    public int salesOverAverage(int[] sales) {
+        int overAverage = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < 15) {
+                continue;
+            } else {
+
+                overAverage += 1;// количество месяцев продаж выше среднего значения
+            }
+        }
+
+        return overAverage;
+    }
+}
+
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,71 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class StatsServiceTest {
+
+    @Test
+    void shouldCalculateMinMonthSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedMinMonth = 9;
+        int actualMinMonth = service.minSales(sales);
+        Assertions.assertEquals(expectedMinMonth , actualMinMonth);
+    }
+
+    @Test
+    void shouldCalculateMaxMonthSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedMaxMonth = 8;
+        int actualMaxMonth = service.maxSales(sales);
+        Assertions.assertEquals(expectedMaxMonth, actualMaxMonth);
+    }
+
+    @Test
+    void shouldCalculateAmountSales() {
+        StatsService service = new StatsService();
+        int sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedAmount = 180;
+        int actualAmount = service.amountSales(sales);
+
+        Assertions.assertEquals(expectedAmount, actualAmount);
+
+
+    }
+
+    @Test
+    void shouldCalculateAverageAmountSales() {
+        StatsService service = new StatsService();
+        int sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedAverageAmount = 15;
+        int actualAverageAmount = service.averageSales(sales);
+
+        Assertions.assertEquals(expectedAverageAmount, actualAverageAmount);
+    }
+
+    @Test
+    void shouldCalculateUnderAverageAmountSales() {
+        StatsService service = new StatsService();
+        int sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedUnderAverageAmount = 5;
+        int actualUnderAverageAmount = service.salesUnderAverage(sales);
+
+        Assertions.assertEquals(expectedUnderAverageAmount, actualUnderAverageAmount);
+    }
+
+    @Test
+    void shouldCalculateOverAverageAmountSales() {
+        StatsService service = new StatsService();
+        int sales[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedOverAverageAmount = 7;
+        int actualOverAverageAmount = service.salesOverAverage(sales);
+
+        Assertions.assertEquals(expectedOverAverageAmount, actualOverAverageAmount);
+    }
+}
+
+
+
